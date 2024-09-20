602969c5cf0890bdda1341e6e3f64f77ca7af49d
Aristo Hadisoeganda <aristo.hadisoeganda@gmail.com>
2024-09-20 20:11:44 +0700

    add where condition

--- src/main/java/com/kinnarastudio/kecakplugins/openbravo/form/OpenbravoOptionsBinder.java
+++ src/main/java/com/kinnarastudio/kecakplugins/openbravo/form/OpenbravoOptionsBinder.java
@@ -1,28 +1,14 @@
 package com.kinnarastudio.kecakplugins.openbravo.form;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Base64;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
-import java.util.ResourceBundle;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
+import com.kinnarastudio.commons.Try;
+import com.kinnarastudio.commons.jsonstream.JSONStream;
+import com.kinnarastudio.kecakplugins.openbravo.commons.RestMixin;
+import com.kinnarastudio.kecakplugins.openbravo.exceptions.OpenbravoClientException;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.joget.apps.app.service.AppUtil;
-import org.joget.apps.datalist.model.DataListCollection;
-import org.joget.apps.form.model.Element;
-import org.joget.apps.form.model.FormAjaxOptionsBinder;
-import org.joget.apps.form.model.FormBinder;
-import org.joget.apps.form.model.FormData;
-import org.joget.apps.form.model.FormLoadOptionsBinder;
-import org.joget.apps.form.model.FormRow;
-import org.joget.apps.form.model.FormRowSet;
+import org.joget.apps.form.model.*;
 import org.joget.commons.util.LogUtil;
 import org.joget.plugin.base.PluginManager;
 import org.joget.workflow.model.WorkflowAssignment;
@@ -32,12 +18,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import javax.annotation.Nullable;
-import com.kinnarastudio.commons.Try;
-import com.kinnarastudio.commons.jsonstream.JSONStream;
-import com.kinnarastudio.kecakplugins.openbravo.commons.RestMixin;
-import com.kinnarastudio.kecakplugins.openbravo.exceptions.OpenbravoClientException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class OpenbravoOptionsBinder extends FormBinder implements FormLoadOptionsBinder, RestMixin, FormAjaxOptionsBinder{
+public class OpenbravoOptionsBinder extends FormBinder implements FormLoadOptionsBinder, RestMixin, FormAjaxOptionsBinder {
     final public static String LABEL = "Openbravo Options Binder";
 
     @Override
@@ -51,44 +38,34 @@ public class OpenbravoOptionsBinder extends FormBinder implements FormLoadOption
         try {
             final Map<String, String> headers = Collections.singletonMap("Authorization", getAuthenticationHeader(getPropertyUsername(), getPropertyPassword()));
 
-            final StringBuilder url = new StringBuilder(getApiEndPoint(getPropertyBaseUrl(), getPropertyTableEntity()));
-
-            final HttpUriRequest request = getHttpRequest(workflowAssignment, url.toString(), "GET", headers);
+            final String url = getApiEndPoint(getPropertyBaseUrl(), getPropertyTableEntity(), 1000, getWhereCondition());
+            final HttpUriRequest request = getHttpRequest(workflowAssignment, url, "GET", headers);
             final HttpClient client = getHttpClient(isIgnoreCertificateError());
-            LogUtil.info(getClassName(), "Request Option Binder: " + request.toString());
             final HttpResponse response = client.execute(request);
 
-            LogUtil.info(getClassName(), "Response Option Binder: " + response.toString());
-            // final int statusCode = getResponseStatus(response);
-            // if(statusCode == 404) {
-            //     LogUtil.debug(getClassName(), "ID [" + primaryKey + "] : No record");
-            //     return null;
-            // } else if (getStatusGroupCode(statusCode) != 200) {
-            //     throw new OpenbravoClientException("ID [" + primaryKey + "] : Response code [" + statusCode + "] is not 200 (Success)");
-            // } else if (statusCode != 200) {
-            //     LogUtil.warn(getClassName(), "ID [" + primaryKey + "] : Response code [" + statusCode + "] is considered as success");
-            // }
-
-            // if (!isJsonResponse(response)) {
-            //     throw new OpenbravoClientException("Content type is not JSON");
-            // }
-
             try (BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent()))) {
-                final JSONObject jsonResponseObject = new JSONObject(br.lines().collect(Collectors.joining()));
-                final FormRowSet rowSet = new FormRowSet();
-                JSONArray jsonData = jsonResponseObject.getJSONObject("response").getJSONArray("data");
-                
-                JSONStream.of(jsonData, Try.onBiFunction(JSONArray::getJSONObject))
-                .forEach(t -> {
-                    FormRow formRow = new FormRow();
-                    try {
-                        formRow.setProperty("value", t.getString(getPropertyString("valueColumn")));
-                        formRow.setProperty("label", t.getString(getPropertyString("labelColumn")));
-                        rowSet.add(formRow);
-                    } catch (JSONException e) {
-                        LogUtil.error(getClassName(), e, e.getMessage());
-                    }
-                });
+                final String responseString = br.lines().collect(Collectors.joining());
+                final JSONObject jsonResponseObject = new JSONObject(responseString);
+
+                JSONArray jsonData = jsonResponseObject
+                        .getJSONObject("response")
+                        .getJSONArray("data");
+
+                final FormRowSet rowSet = JSONStream.of(jsonData, Try.onBiFunction(JSONArray::getJSONObject))
+                        .map(Try.onFunction(t -> {
+                            FormRow formRow = new FormRow();
+                            final String value = t.getString(getPropertyString("valueColumn"));
+                            final String label = t.getString(getPropertyString("labelColumn"));
+
+                            formRow.setProperty("value", value);
+                            formRow.setProperty("label", label.isEmpty() ? value : label);
+
+                            return formRow;
+                        }))
+                        .filter(Objects::nonNull)
+                        .collect(Collectors.toCollection(FormRowSet::new));
+
+                rowSet.setMultiRow(true);
 
                 return rowSet;
             }
@@ -106,10 +83,21 @@ public class OpenbravoOptionsBinder extends FormBinder implements FormLoadOption
         return AppUtil.processHashVariable(getPropertyString("tableEntity"), null, null, null);
     }
 
-    protected String getApiEndPoint(String baseUrl, String tableEntity) {
-        return baseUrl + "/org.openbravo.service.json.jsonrest/" + tableEntity + "?_endRow=1000";
+    protected String getApiEndPoint(String baseUrl, String tableEntity, int endRow, String where) {
+
+        StringBuilder url = new StringBuilder(baseUrl + "/org.openbravo.service.json.jsonrest/" + tableEntity + "?");
+
+        if(endRow > 0) {
+            url.append("_endRow=").append(endRow).append("&");
+        }
+
+        if(where != null && !where.isEmpty()) {
+            url.append("_where=").append(where).append("&");
+        }
+
+        return url.toString();
     }
-    
+
     protected String getPropertyUsername() {
         return AppUtil.processHashVariable(getPropertyString("username"), null, null, null);
     }
@@ -168,5 +156,9 @@ public class OpenbravoOptionsBinder extends FormBinder implements FormLoadOption
     @Override
     public boolean useAjax() {
         return true;
+    }
+
+    protected String getWhereCondition() {
+        return getPropertyString("whereCondition");
     }
 }
